308cbc08752c0719dc4d4b451221f248d5732ccd
antoine.leveugle <antoine.leveugle@gmail.com>
2016-01-12 17:31:39 +0100

    fix Nest function should returns Promises, since the refresh function is asynchronous. Also remove the api URL from Nest constructor. The Nest should hide this parameter.

--- example/nest-cli.js
+++ example/nest-cli.js
@@ -2,14 +2,12 @@ var Nest = require('../lib/nest');
 
 var token = 'PLEASE INSERT TOKEN HERE https://github.com/denouche/node-nest-oauth2 if you need to retrieve it';
 
-var url = 'https://developer-api.nest.com/devices';
-var myNest = new Nest(url, token);
+var myNest = new Nest(token);
 
-   myNest.refresh()
-   .then(function(data) {
-   	console.log(myNest.getStatus());
-   }, function(error) {
-   	console.error(error);
-   });
-
+myNest.refresh()
+    .then(function(data) {
+   		console.log(data);
+    }, function(error) {
+	   	console.error(error);
+    });
 
